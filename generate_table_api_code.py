b50c15ebb3ab711e0e602215690f4436a4cc6ad8
Rok Kogovšek <rok.kogovsek@xlab.si>
2014-11-03 12:12:20 +0100

    Combined rows of same category
    The firs coloumn combines the rows of same category

--- generate_table_api_code.py
+++ generate_table_api_code.py
@@ -10,6 +10,8 @@ for category in categories:
 		category_name = category_name[0].replace("\n","").strip()
 		methods = category.xpath(".//div[@class='resource2']")
 		if methods:
+			print "<tr><td rowspan='%d'>%s</td>" % (len(methods), category_name)
+			is_first_method = True
 			for method in methods:
 				method_name = method.xpath(".//h2[@class='resource2Name']/text()")
 				if method_name:
@@ -22,6 +24,8 @@ for category in categories:
 					method_description = method_description[0].replace("\n","").strip()
 				else:
 					method_description = "/"
-				print "<tr><td>%s</td><td>%s</td><td>%s</td></tr>" % (category_name, method_name, method_description)
+				if not is_first_method:
+					print "<tr>"
+				print "<td>%s</td><td>%s</td></tr>" % (method_name, method_description)
 		else:
 			print "<tr><td>%s</td><td>/</td><td>/</td></tr>" % category_name
