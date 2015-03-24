8ea42fe23a168d46279391a9857549306c7e50d5
Pedro Kostelec <pedro.kostelec@xlab.si>
2015-03-24 12:25:31 +0100

    Separate DSEs by option

---
+++ js/load_list.js
@@ -0,0 +1,83 @@
+(function loadList(){
+	/* global $ */
+	"use strict";
+	var enabler_list = $(".enabler-list");
+	var dataGlobal = [];
+
+	/*
+	Option 1: the specification of the DSE is made publically available, royalty free.
+	Option 2: the specification of the DSE is made publically available, royalty free and the code is available Open Source.
+	*/
+	function getUrlParameter(sParam)
+	{
+	    var sPageURL = window.location.search.substring(1);
+	    var sURLVariables = sPageURL.split('&');
+	    for (var i = 0; i < sURLVariables.length; i++) 
+	    {
+	        var sParameterName = sURLVariables[i].split('=');
+	        if (sParameterName[0] == sParam) 
+	        {
+	            return sParameterName[1];
+	        }
+	    }
+	}    
+	var option = getUrlParameter('option') || '1';
+
+	if (option == '2') {
+		$('.js-title').html('Other FINESCE results')
+	}
+
+	// This causse DSEs.json to be refreshed every hour
+	var cacheBuster = Math.floor(new Date().getTime() / (1000 * 60 * 60));
+	$.getJSON("js/json/DSEs.json?ts=" + cacheBuster, function(data){
+		data.dse = data.dse.filter(function(dse) {
+			return dse.option == option;
+		});
+
+		data = data
+
+		dataGlobal = data;
+
+		showData("all");
+
+		data = data.dse;
+		var wp_list = [];
+		for (var line in data){
+			wp_list[data[line].site] = true;
+		}
+		//fill dropdown
+		$(".dropdown-menu").empty();
+		$(".dropdown-menu").append('<li role="presentation"><a role="menuitem" tabindex="-1" data-id="all" href="#">All</a></li>');
+		$("#dropdownMenu1").html('All <span class="caret"></span>');
+
+		for (var wp in wp_list){
+			$(".dropdown-menu").append('<li role="presentation"><a role="menuitem" tabindex="-1" data-id="'+wp+'" href="#">'+wp+'</a></li>');
+		}
+	});
+
+	$(".dropdown-menu").on("click", "li a", function(){
+		var selText = $(this).text();
+		$("#dropdownMenu1").html(selText+' <span class="caret"></span>');
+		showData($(this).data('id'));
+	});
+
+	function showData (filter) {
+		$(".enabler-row").remove();
+
+		// enabler_list.html('');
+		var row_html = "";
+		var data = dataGlobal;
+		var data = data.dse;
+
+		if(filter && filter !== "all") {
+			data = data.filter(function(dt){
+				return dt.site === filter;
+			});
+		}
+
+		for (var line in data) {
+			row_html = '<div class="enabler-row"><h2 class="header"><a href="DSE.html?id='+ data[line].id + '">'+data[line].name+'</a></h2>'+data[line].description+'<div class="category">'+data[line].site+'</div></div>';
+			enabler_list.append(row_html);
+		}
+	}
+})();
